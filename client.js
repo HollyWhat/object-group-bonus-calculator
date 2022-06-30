b871da9dd6ac12d376d241f2070990a25286fbf1
Brianna Oliveira <mystikgab@gmail.com>
2022-06-30 11:28:04 -0500

    completed required questions

--- client.js
+++ client.js
@@ -60,6 +60,8 @@ for(let i=0; i<employees.length; i += 1){
 function bonusCalculator(employeeInput){
   console.log('in bonusCalculator:', employeeInput);
   let bonusPercentage = 0;
+  let salaryNumber = Number.parseInt(employeeInput.annualSalary)
+  // let employeeNumber = Number.parseInt(employeeInput.employeeNumber)
   // begin if statements for cal
 
 if( employeeInput.reviewRating <= 2){
@@ -74,15 +76,37 @@ if( employeeInput.reviewRating <= 2){
   console.log("your bonus is 10%");
   bonusPercentage += 1.10;
 }
-// make totalCompensation another function that we call here
-  const newObject = {
+// new if statement
+if(employeeInput.employeeNumber.length === 4){
+  console.log("your bonus is increased by 5%");
+  bonusPercentage += 0.05;
+}
+if(employeeInput.annualSalary > 65000){
+  console.log("bonus adjusted down 1%");
+  bonusPercentage -= 0.01;
+}
+if(bonusPercentage > 1.13){
+  bonusPercentage = 1.13;
+}
+if(bonusPercentage < 0){
+  bonusPercentage = 1.0;
+}
+
+console.log('bonus percentage', bonusPercentage);
+let totalCompensation = Math.round(salaryNumber * bonusPercentage);
+console.log('total compensation', totalCompensation);
+let totalBonus = Math.round(totalCompensation - salaryNumber);
+console.log('total bonus', totalBonus);
+const newObject = {
     name: employeeInput.name,
     bonusPercentage: bonusPercentage,
     totalCompensation: totalCompensation,
     totalBonus: totalBonus,
   };
+  console.log(newObject);
   return newObject;
   }
 
-
-
+for(let i=0; i<employees.length; i++){
+bonusCalculator(employees[i]);
+}
